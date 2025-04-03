50506e5c033cea30054943f76579ca78ef67d63a
Robert Mastalerek <robert.mastalerek@gmail.com>
2025-04-03 17:09:54 +0200

    unignore test

--- qa/integration-tests-webapps/integration-tests/src/main/java/org/eximeebpms/bpm/LoginIT.java
+++ qa/integration-tests-webapps/integration-tests/src/main/java/org/eximeebpms/bpm/LoginIT.java
@@ -31,7 +31,6 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-@Ignore("TODO - rmastalerek")
 public class LoginIT extends AbstractWebappUiIntegrationTest {
 
   protected WebDriverWait wait;

69563ffc6468d2e4825244772899c7ddc3a954dd
Robert Mastalerek <robert.mastalerek@gmail.com>
2025-04-11 15:57:23 +0200

    chore: kod GTM

--- engine-rest/docs/build.js
+++ engine-rest/docs/build.js
@@ -39,8 +39,8 @@ async function generateDocs() {
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://ssgtm.camunda.org/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PP9MHKW');</script>
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N39WSFRC');</script>
     <!-- End Google Tag Manager -->
 
     <meta charset="utf8"/>
@@ -69,6 +69,9 @@ async function generateDocs() {
 </head>
 
 <body>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N39WSFRC" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <div id="redoc">${html}</div>
 <script>
     var container = document.getElementById('redoc');

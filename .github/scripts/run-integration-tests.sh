3dca5472607f0244ba3a075eb7a4f3f7af938cdb
Robert Mastalerek <robert.mastalerek@gmail.com>
2025-05-13 16:01:59 +0200

    fix: integration tests workflow fix

--- .github/scripts/run-integration-tests.sh
+++ .github/scripts/run-integration-tests.sh
@@ -66,7 +66,7 @@ run_build () {
   fi
 
   echo "ℹ️ Building $TEST_SUITE integration tests for distro $DISTRO with $DATABASE database using profiles: [${PROFILES[*]}]"
-  echo "./mvnw -DskipTests -P$(IFS=,; echo "${PROFILES[*]}") clean install"
+  echo "./mvnw -DskipTests -P$(IFS=,; echo "${PROFILES[*]}"),distro-ce clean install"
   ./mvnw -DskipTests -P$(IFS=,; echo "${PROFILES[*]}") clean install
   if [[ $? -ne 0 ]]; then
     echo "❌ Error: Build failed"
@@ -113,7 +113,7 @@ run_tests () {
   esac
 
   echo "ℹ️ Running $TEST_SUITE integration tests for distro $DISTRO with $DATABASE database using profiles: [${PROFILES[*]}]"
-  echo "./mvnw -P$(IFS=,; echo "${PROFILES[*]}") clean verify -f $QA_DIR"
+  echo "./mvnw -P$(IFS=,; echo "${PROFILES[*]}"),distro-ce clean verify -f $QA_DIR"
   ./mvnw -P$(IFS=,; echo "${PROFILES[*]}") clean verify -f $QA_DIR
   if [[ $? -ne 0 ]]; then
     echo "❌ Error: Build failed"

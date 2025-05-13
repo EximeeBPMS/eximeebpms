ee7b843a0352d33c388934e77e80763c617267fb
Konasiek <konasiek@gmail.com>
2025-05-13 12:26:53 +0200

    just sonar test

--- engine/src/main/java/org/eximeebpms/bpm/engine/delegate/BaseDelegateExecution.java
+++ engine/src/main/java/org/eximeebpms/bpm/engine/delegate/BaseDelegateExecution.java
@@ -23,7 +23,7 @@ package org.eximeebpms.bpm.engine.delegate;
  * @author Sebastian Menski
  *
  */
-public interface BaseDelegateExecution extends VariableScope {
+public interface BaseDelegateExecution extends VariableScope  {
 
   /** Unique id of this path of execution that can be used as a handle to provide external signals back into the engine after wait states. */
   String getId();

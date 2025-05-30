b1d798c2f06da2bd527e5000f8f29e8cc280b400
Konasiek <konasiek@gmail.com>
2025-05-30 09:34:20 +0200

    using ThreadLocalRandom

--- engine/src/main/java/org/eximeebpms/bpm/engine/impl/jobexecutor/BackoffJobAcquisitionStrategy.java
+++ engine/src/main/java/org/eximeebpms/bpm/engine/impl/jobexecutor/BackoffJobAcquisitionStrategy.java
@@ -19,6 +19,7 @@ package org.eximeebpms.bpm.engine.impl.jobexecutor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * <p>Determines the number of jobs to acquire and the time to wait between acquisition cycles
@@ -286,7 +287,7 @@ public class BackoffJobAcquisitionStrategy implements JobAcquisitionStrategy {
     if (applyJitter) {
       // add a bounded random jitter to avoid multiple job acquisitions getting exactly the same
       // polling interval
-      backoffTime += (long)(Math.random() * ((double) backoffTime / 2));
+      backoffTime += ThreadLocalRandom.current().nextLong(backoffTime / 2);
     }
 
     return backoffTime;

42f45b344d4ec98dc17ba23b884d18d08b5348c6
Robert Mastalerek <robert.mastalerek@gmail.com>
2025-06-02 15:50:17 +0200

    fix(engine): fix initial retry counter (#5151)

--- engine/src/test/java/org/eximeebpms/bpm/engine/test/api/queries/QueryByIdAfterTest.java
+++ engine/src/test/java/org/eximeebpms/bpm/engine/test/api/queries/QueryByIdAfterTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.engine.test.api.queries;
+package org.eximeebpms.bpm.engine.test.api.queries;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
